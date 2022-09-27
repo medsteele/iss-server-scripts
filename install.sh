9b0a81b65489d85a97e8d0504b65144c8b05973a
medsteele <104858841+medsteele@users.noreply.github.com>
2022-09-27 09:32:15 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
 
+# upgrade system
+apt update && apt upgrade -y
+
+# install nginx & jq & curl
+apt install nginx jq curl -y
+
+# create scripts director
+rm -r /scripts
+mkdir /scripts
+
+# configure nginx
+#rm -r /var/www/html/
+
+# create script file & index page
+# wget from github
+
+# create crontab
+echo "* * * * * sh /scripts/script.sh" >> /var/tmp/cronc>
+crontab /var/tmp/cronconfig
+rm /var/tmp/cronconfig
