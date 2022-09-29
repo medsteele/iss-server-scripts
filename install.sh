854c97f4cfd0f2fab45c7984289b41b5e13986ee
medsteele <104858841+medsteele@users.noreply.github.com>
2022-09-29 09:51:23 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@ apt update && apt upgrade -y
 apt install nginx jq curl -y
 
 # configure nginx
-rm -r /var/www/html/*
+#rm -r /var/www/html/*
 wget -O /var/www/html/index.html https://raw.githubusercontent.com/medsteele/iss-server-scripts/main/index.html
 
 # create scripts directory & download scripts
-rm -r /scripts
+#rm -r /scripts
 mkdir /scripts
 wget -O /scripts/install.sh https://raw.githubusercontent.com/medsteele/iss-server-scripts/main/install.sh
 wget -O /scripts/update-webpage.sh https://raw.githubusercontent.com/medsteele/iss-server-scripts/main/update-webpage.sh
