7a3fb61b7c4f900870e19b6df7fbd7986c36dcc4
medsteele <104858841+medsteele@users.noreply.github.com>
2022-09-29 09:37:44 -0400

    Update update-webpage.sh

--- update-webpage.sh
+++ update-webpage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "current datetime: $(date) : " >> /var/www/html/index.html
+echo "tracker datetime 9/29: $(date) : " >> /var/www/html/index.html
 curl http://api.open-notify.org/iss-now.json | jq .iss_position >> /var/www/html/index.html
 echo "<br/>" >> /var/www/html/index.html
